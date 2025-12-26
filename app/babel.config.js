bd99843c7fb1a79c0d4ed1a046bc6f08d03b832e
davidojo1144 <davco1144@gmail.com>
2025-12-26 21:05:33 +0100

    Resilient-Euro-Transfer-System-Mobile-App

--- app/babel.config.js
+++ app/babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // IMPORTANT: Reanimated plugin must be listed last
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [],
   };
 };
